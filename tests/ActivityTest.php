7cafcd22874bda6169c2c8817d547b12523d5ccc
cedric frossard <adrion.dev@gmail.com>
2017-01-23 16:35:38 +0100

    add unit tests

---
+++ tests/ActivityTest.php
@@ -0,0 +1,13 @@
+<?php namespace HON\HonCuratorUser\Models;
+
+use HON\HonCuratorUser\Models\Activity;
+use PluginTestCase;
+
+class PostTest extends PluginTestCase
+{
+    public function testGetActivityOptions()
+    {
+        $activities = Activity::getActivityOptions();
+        $this->assertEquals(4, count($activities), "We should have 4 activities after seeding");
+    }
+}
