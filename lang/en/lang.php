fd80f6a036ba03cd9069857f7b2c8a306e134d78
cedric frossard <adrion.dev@gmail.com>
2017-09-08 12:38:16 +0200

    add missing key

--- lang/en/lang.php
+++ lang/en/lang.php
@@ -13,6 +13,7 @@ return [
     ],
     'account' => [
         'welcomeSubject' => 'Your account has been confirmed!',
-        'welcome1' => 'This is a message to inform you that your account has been activated successfully. By default option, your selected activity is "Patient". From your first connexion, go to the My Account part of the website to select the correct activity.'
+        'welcome1' => 'This is a message to inform you that your account has been activated successfully. By default option, your selected activity is "Patient". From your first connexion, go to the My Account part of the website to select the correct activity.',
+        'hello' => 'Hello '
     ]
 ];
--- lang/fr/lang.php
+++ lang/fr/lang.php
@@ -13,6 +13,7 @@ return [
     ],
     'account' => [
         'welcomeSubject' => 'Confirmation de votre inscription!',
-        'welcome1' => "Ce message vous informe que votre compte a été créé avec succès. Par défaut, votre activité sélectionnée est \'Patient\'. Dès votre première connexion, rendez-vous sur la partie Mon compte du site afin de choisir l\'activité qui vous correspond. "
+        'welcome1' => "Ce message vous informe que votre compte a été créé avec succès. Par défaut, votre activité sélectionnée est \'Patient\'. Dès votre première connexion, rendez-vous sur la partie Mon compte du site afin de choisir l\'activité qui vous correspond. ",
+        'hello' => 'Bonjour '
     ]
 ];
